5ed4e5e03eb82d56ebb03e0aca91f3e05a5d6165
misspatriana <justinquidit5@gmail.com>
2025-03-30 12:56:26 +0800

    add profile

---
+++ src/pages/Profile.tsx
@@ -0,0 +1,28 @@
+import { 
+    IonButtons,
+      IonContent, 
+      IonHeader, 
+      IonMenuButton, 
+      IonPage, 
+      IonTitle, 
+      IonToolbar 
+  } from '@ionic/react';
+  
+  const Profile: React.FC = () => {
+    return (
+      <IonPage>
+        <IonHeader>
+          <IonToolbar>
+            <IonButtons slot='start'>
+              <IonMenuButton></IonMenuButton>
+            </IonButtons>
+            <IonTitle>Profile</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent fullscreen>
+        </IonContent>
+      </IonPage>
+    );
+  };
+
+  export default Profile;
