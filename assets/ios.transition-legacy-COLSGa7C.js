e2385b71df7001fc2f855c182114bd12fe33ac4f
misspatriana <justinquidit5@gmail.com>
2025-04-24 02:54:41 +0800

    Updates

--- assets/ios.transition-legacy-COLSGa7C.js
+++ assets/ios.transition-legacy-COLSGa7C.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CnedVklr.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy--6D4WS93.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
