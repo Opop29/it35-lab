b26dbb36f5afa1836b563179cbfee49b90dc7034
misspatriana <justinquidit5@gmail.com>
2025-04-24 01:35:27 +0800

    Updates

--- assets/md.transition-legacy-BCyVdoQn.js
+++ assets/md.transition-legacy-BCyVdoQn.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CiHB7MOd.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
+System.register(["./index-legacy-CAMNLvOP.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.o,i=e.n}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-D2ULAcXy.js
+++ assets/swipe-back-legacy-D2ULAcXy.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CiHB7MOd.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CAMNLvOP.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
